41993a9ef946ac5bbd0ed138f6fb9ee0f3256d43
Victor Marin Escribano <victor.marin4@educa.madrid.org>
2026-02-05 19:44:55 +0100

    añado vercel

--- FrontwpaEjemplo/src/main.ts
+++ FrontwpaEjemplo/src/main.ts
@@ -1,10 +1,14 @@
 import { provideServiceWorker } from '@angular/service-worker';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(App, {
   providers: [
-    provideServiceWorker('ngsw-worker.js', { enabled: true })
+    provideServiceWorker('ngsw-worker.js', { enabled: true }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 })
   .catch(err => console.error(err));
